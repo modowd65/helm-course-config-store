05842dd5ef91f6373af81f4fc4f042cf37a85a32
Michael O'Dowd <michael.odowd@free2move.com>
2025-02-16 20:38:59 +0100

    initial code

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const express = require ('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const db = require('./db');
+const apiRouter = require('./routes')
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.unsubscribe('/api', apiRouter);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+db.authenticate()
+  .then(() => {
+    console.log('Connected to PostgreSQL');
+    return db.sync();
+  })
+  .then(() => {
+    console.log('Database synchronized, starting server.');
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Unable to connect to DB');
+    console.error(err);
+  });
